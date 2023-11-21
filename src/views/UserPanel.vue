f6b6768d4cf4efdb4a23e553eef96dca65343866
Погадайко Антон <pogadajko52@gmail.com>
2023-11-21 20:49:48 +0200

    refactored UserPanel page

--- src/views/UserPanel.vue
+++ src/views/UserPanel.vue
@@ -5,8 +5,8 @@
         <h1 class="panel__title">Users panel:</h1>
         <v-alert
           v-if="errorAlert.show"
-          type="error"
           :text="errorAlert.message"
+          type="error"
         ></v-alert>
         <users-list :users="users" :loading="isLoading" @edit-user="editUser" />
       </div>
@@ -18,17 +18,29 @@
 import { ref, onMounted } from "vue";
 import { useStore } from "vuex";
 import { IAlert } from "../models/alert.ts";
-import UsersList from "../components/UsersList.vue";
 import { IUser, IUsers } from "../models/user";
+import UsersList from "../components/UsersList.vue";
 
 const store = useStore();
 
-const users = ref<any>([]);
+const users = ref<IUsers>([]);
 const isLoading = ref<boolean>(false);
-
 const errorAlert = ref<IAlert>({
   show: false,
   message: "",
+});
+
+onMounted(async (): Promise<void> => {
+  if (!localStorage.getItem("users")) {
+    const response = await getUsers();
+    if (response) {
+      users.value = response;
+      localStorage.setItem("users", JSON.stringify(users.value));
+      return;
+    }
+  }
+
+  users.value = JSON.parse(localStorage.getItem("users") || "");
 });
 
 async function getUsers(): Promise<IUsers | void> {
@@ -40,23 +52,13 @@ async function getUsers(): Promise<IUsers | void> {
         show: true,
         message: response.message,
       };
-      return;
+      return [];
     }
     return response;
   } finally {
     isLoading.value = false;
   }
 }
-
-onMounted(async () => {
-  if (!localStorage.getItem("users")) {
-    users.value = await getUsers();
-    localStorage.setItem("users", JSON.stringify(users.value));
-    return;
-  }
-
-  users.value = JSON.parse(localStorage.getItem("users") || "");
-});
 
 function editUser(editedUser: IUser): void {
   const index = users.value.findIndex(
@@ -67,15 +69,4 @@ function editUser(editedUser: IUser): void {
 }
 </script>
 
-<style scoped lang="scss">
-.panel {
-  height: 100%;
-  padding: 80px 0 30px;
-
-  &__title {
-    font-size: 1.5rem;
-    font-weight: 600;
-    margin-bottom: 20px;
-  }
-}
-</style>
+<style scoped src="../assets/styles/views/panel.scss"></style>

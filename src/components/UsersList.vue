e6802cd05a4552b28855899a796922fbc6b24830
Погадайко Антон <pogadajko52@gmail.com>
2023-11-21 18:59:01 +0200

    added UsersList

---
+++ src/components/UsersList.vue
@@ -0,0 +1,25 @@
+<template>
+  <loader v-if="loading" />
+  <v-item-group v-else>
+    <v-row>
+      <v-col v-for="user in users" :key="user.id" cols="12" md="4">
+        <v-item>
+          <user-card :user="user"></user-card>
+        </v-item>
+      </v-col>
+    </v-row>
+  </v-item-group>
+</template>
+
+<script setup lang="ts">
+import { IUsers } from "../models/user";
+import UserCard from "../components/UserCard.vue";
+import Loader from "./UI/Loader.vue";
+
+interface IProps {
+  users: IUsers;
+  loading: boolean;
+}
+
+defineProps<IProps>();
+</script>

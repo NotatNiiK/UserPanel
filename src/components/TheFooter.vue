58e7878018bc6f9b4c2ed7baff54f9a354bcdd24
Погадайко Антон <pogadajko52@gmail.com>
2023-11-21 17:10:56 +0200

    temporary fixed footer height bug

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-footer color="primary" class="bg-grey-lighten-1" height="50">
+  <v-footer color="primary" height="50" style="flex: 0 0 50px">
     <v-row justify="center" no-gutters>
       <v-col class="text-center" cols="12">
         © 2023 Anton's Fortune Teller. All rights reserved.

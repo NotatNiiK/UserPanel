9eb7a955d3dc0f502bb3db9c1231d622cd0921b7
Погадайко Антон <pogadajko52@gmail.com>
2023-11-21 17:10:38 +0200

    added nav links

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,19 @@
 <template>
-  <v-app-bar color="primary" :elevation="2">
-    <v-app-bar-title>User Panel Application</v-app-bar-title>
+  <v-app-bar color="primary" :elevation="2" class="header">
+    <v-container>
+      <v-row justify="space-between">
+        <v-col>
+          <v-app-bar-title>User Panel Application</v-app-bar-title>
+        </v-col>
+        <v-col align="end">
+          <v-btn class="ml-2">
+            <RouterLink to="/">Home</RouterLink>
+          </v-btn>
+          <v-btn class="ml-2">
+            <RouterLink to="/panel">Panel</RouterLink>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app-bar>
 </template>

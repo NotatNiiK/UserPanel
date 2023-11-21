61d6c7e76fb61d9cc4ab71b728a24a6fb6794ab1
Погадайко Антон <pogadajko52@gmail.com>
2023-11-21 20:35:07 +0200

    refactored header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,16 +1,16 @@
 <template>
-  <v-app-bar color="primary" :elevation="2" class="header">
+  <v-app-bar color="primary">
     <v-container>
-      <v-row justify="space-between">
+      <v-row justify="space-between" align="center">
         <v-col>
           <v-app-bar-title>User Panel Application</v-app-bar-title>
         </v-col>
         <v-col align="end">
-          <v-btn class="ml-2">
-            <RouterLink to="/">Home</RouterLink>
+          <v-btn class="ml-1" size="small" @click="$router.push('/')">
+            Home
           </v-btn>
-          <v-btn class="ml-2">
-            <RouterLink to="/panel">Panel</RouterLink>
+          <v-btn class="ml-1" size="small" @click="$router.push('/panel')">
+            Panel
           </v-btn>
         </v-col>
       </v-row>

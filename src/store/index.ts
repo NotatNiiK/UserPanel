811d09b1b1db720052f0821c7c821b5f540f0500
Погадайко Антон <pogadajko52@gmail.com>
2023-11-21 18:07:12 +0200

    added UserSerice request to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,44 @@
-import { createStore } from "vuex";
+import { createStore, Commit, Dispatch } from "vuex";
+import { IUsers } from "../models/user";
+import { IPageParams } from "../models/api.response";
+import { AxiosError } from "axios";
+import UserService from "../services/UserService";
 
-export default createStore({
+interface State {
+  users: IUsers;
+}
+
+interface MyContext {
+  commit: Commit;
+  dispatch: Dispatch;
+}
+
+export default createStore<State>({
   state: {
-    message: 0,
+    users: [],
   },
-  mutations: {},
-  actions: {},
+
+  mutations: {
+    setUsers(state: State, newUsers: IUsers) {
+      state.users = newUsers;
+    },
+  },
+
+  actions: {
+    async getUsers(
+      { commit }: MyContext,
+      pageParams: IPageParams
+    ): Promise<void> {
+      try {
+        const {
+          data: { data },
+        } = await UserService.getUsers(pageParams);
+        commit("setUsers", data);
+      } catch (e: unknown) {
+        const error = e as AxiosError;
+        console.log(error);
+      }
+    },
+  },
   getters: {},
 });
